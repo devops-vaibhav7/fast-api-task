7612355f32055647a43ddcd86aabbb4c08fe17f6
Devops-Vaibhav7 <devopsvaibhav7@gmail.com>
2026-05-26 23:37:10 +0530

    Updated

---
+++ secrets-loader.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Fetching secrets from AWS Secrets Manager..."
+
+SECRET_JSON=$(aws secretsmanager get-secret-value \
+  --secret-id Fast-API-Secrets \
+  --region us-east-1 \
+  --query SecretString \
+  --output text)
+
+if [ $? -ne 0 ]; then
+  echo "Failed to fetch secrets!"
+  exit 1
+fi
+
+export POSTGRES_DB=$(echo $SECRET_JSON | jq -r .POSTGRES_DB)
+export POSTGRES_USER=$(echo $SECRET_JSON | jq -r .POSTGRES_USER)
+export POSTGRES_PASSWORD=$(echo $SECRET_JSON | jq -r .POSTGRES_PASSWORD)
+export REDIS_HOST=$(echo $SECRET_JSON | jq -r .REDIS_HOST)
+
+echo "Secrets loaded successfully."
+
+docker compose down
+
+docker compose up -d --build
